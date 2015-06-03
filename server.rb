7b11f5d127b79b90fb7e392a578e1503773603f5
bala-wegilant <bala@wegilant.com>
2015-06-03 18:04:06 +0530

    Update server.rb

--- server.rb
+++ server.rb
@@ -2,6 +2,14 @@ require 'sinatra'
 require 'json'
 
 post '/payload' do
-  push = JSON.parse(request.body.read)
+  request.body.rewind
+  payload_body = request.body.read
+  verify_signature(payload_body)
+  push = JSON.parse(params[:payload])
   puts "I got some JSON: #{push.inspect}"
+end
+
+def verify_signature(payload_body)
+  signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV['SECRET_TOKEN'], payload_body)
+  return halt 500, "Signatures didn't match!" unless Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
 end
